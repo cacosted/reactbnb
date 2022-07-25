66d1c8386670f17d5b29f1dd561eb8318d1f6964
Clemente Acosta <clems.acosta@gmail.com>
2022-07-24 19:16:40 -0500

    Added mobile style on header

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../assets/logo.svg'
+import { FaSearch } from 'react-icons/fa'
 const NavBar = ({ children }) => {
   return (
     <nav>
@@ -8,23 +9,35 @@ const NavBar = ({ children }) => {
   )
 }
 
-const Logo = () => <img src={logo} alt='reactbnb' />
+const Logo = () => <img className='Header-img' src={logo} alt='reactbnb' />
 
-const Button = ({ children }) => <button>{children}</button>
+const Button = ({ children, filter, bordered }) => {
+  const isBordered = bordered ? 'Header-btn--bordered' : ''
+  return (
+    <button
+      onClick={() => console.log(filter)}
+      className={`Header-btn ${isBordered}`}
+    >
+      {children}
+    </button>
+  )
+}
 
 export const Header = () => {
   return (
     <NavBar>
       <Logo />
-
-      <Button>
-        Helsinki, Finland
-      </Button>
-
-      <Button>
-        <span>Add Guest</span>
-        <span>Search</span>
-      </Button>
+      <div className='Header-search'>
+        <Button filter='location'>
+          Helsinki, Finland
+        </Button>
+        <Button filter='guest' bordered>
+          <span>Add Guest</span>
+        </Button>
+        <Button filter='guest'>
+          <FaSearch color='#eb5757' />
+        </Button>
+      </div>
 
     </NavBar>
   )
