432433a4f79ddcfce259596108aa742ce207ed12
Clemente Acosta <clems.acosta@gmail.com>
2022-08-10 18:07:39 -0500

    Added onSubmit function to hide the modal

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -3,9 +3,13 @@ import { FaSearch } from 'react-icons/fa'
 // import { FilterList } from '../../containers/FilterList'
 // import { GuestItem } from '../GuestItem'
 
-export const SearchBar = ({ type, children }) => {
+export const SearchBar = ({ children, setActiveModal }) => {
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setActiveModal(false)
+  }
   return (
-    <form className='SearchBar' onSubmit={e => e.preventDefault()}>
+    <form className='SearchBar' onSubmit={onSubmit}>
       {children}
       <button className='SearchBar-submit' type='submit'>
         <FaSearch />
